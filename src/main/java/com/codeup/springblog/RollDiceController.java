b7b90792aee5c19fd72960b97886c77f3606caac
Ricky Davis <ricky.c.davis.9@gmail.com>
2019-06-26 14:31:13 -0500

    renamed RollDice Class

--- src/main/java/com/codeup/springblog/RollDiceController.java
+++ src/main/java/com/codeup/springblog/RollDiceController.java
@@ -4,13 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Random;
 
 @Controller
-public class RollDiceServlet {
+public class RollDiceController {
 
     @GetMapping("/roll-dice")
     public String rollDice(){
