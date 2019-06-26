e20cfef09fb38626635b7bd2635ceea25a0a185f
Ricky Davis <ricky.c.davis.9@gmail.com>
2019-06-26 14:47:16 -0500

    Finished last exercise

--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -1,24 +1,35 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+
 @Controller
 public class PostController {
 
     @GetMapping("/posts")
-    @ResponseBody
-    public String getPostsDescription(){
-        return "posts index page";
+    public String getPostsDescription(Model model){
+        ArrayList<Post> postsList = new ArrayList<>();
+        Post post = new Post("Boop","Bop");
+        Post post2 = new Post("beep","bip");
+        postsList.add(post);
+        postsList.add(post2);
+        model.addAttribute("postsList",postsList);
+        return "posts/index";
     }
+
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String getPostByIDDescription(@PathVariable int id){
-        return "view an individual post";
+    public String getPostByIDDescription(@PathVariable int id, Model model){
+        Post post = new Post("Boop","Bop");
+        model.addAttribute("post",post);
+        return "posts/show";
     }
+
     @GetMapping("/posts/create")
     @ResponseBody
     public String getPostCreateDescription(){
@@ -29,6 +40,4 @@ public class PostController {
     public String PostCreatePostDescription(){
         return "create a new post";
     }
-
-
 }
